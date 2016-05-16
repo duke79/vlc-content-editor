e1858fac46f86171faa787b78df2ac7b0fa7579b
Pulkit Singh <pulkitsingh01@gmail.com>
2016-05-16 19:59:44 +0530

    commencer

---
+++ content-editor.lua
@@ -0,0 +1,17 @@
+--[[
+Program: Content Editor
+Purpose: Skip scenes from the movie.
+Author: Pulkit Singh
+License: GNU GENERAL PUBLIC LICENSE
+]]
+
+function descriptor()
+    return { title = "Content Editor",
+             version = "1.0",
+             author = "Pulkit Singh",
+             url = "https://github.com/duke79/vlc-content-editor",
+             shortdesc = "Content Editor",
+             description = "Skip scenes from the movie.",
+             capabilities = {}
+	}
+end
